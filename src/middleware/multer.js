6c4f8f859b359cf8490b476c0baa51fb4108e249
kishanraj04 <kishuraj1111@gmail.com>
2025-07-21 12:55:22 +0530

    creating the instal of multer for uplading files

---
+++ src/middleware/multer.js
@@ -0,0 +1,23 @@
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from 'multer';
+import cloudinary from "../../config/cloudinary.config.js"
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: async (req, file) => {
+    return {
+      folder: 'lms_uploads', // general folder name
+      allowed_formats: ['jpg', 'jpeg', 'png', 'pdf', 'mp4'],
+      public_id: `${Date.now()}-${file.originalname.split('.')[0]}`,
+      resource_type: 'auto', 
+      transformation: [{ width: 500, height: 500, crop: 'limit' }], 
+    };
+  },
+});
+
+const uploader = multer({ storage });
+
+export const uploadSingle = uploader.single('avatar');
+export const uploadMultiple = uploader.array('files', 10); 
+
+// Or export generic uploader for custom use
+export default uploader;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { directLogin, userLogin, userRegister } from '../controller/user.controller.js'
+import { uploadSingle } from '../middleware/multer.js'
 
 const userRouter = express.Router()
 
-userRouter.post("/register",userRegister)
+userRouter.post("/register",uploadSingle,userRegister)
 userRouter.post("/login",userLogin)
 userRouter.get("/directlogin",directLogin)
 export {userRouter}
